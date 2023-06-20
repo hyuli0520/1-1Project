4c7ebb464c695ce3906c6790351b1e87a3444125
minhyeok <minhyeogjeon945@gmail.com>
2023-06-20 15:50:45 +0900

    UpLoad1
    UpLoad1

---
+++ 1-1_Project.c
@@ -0,0 +1,163 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Windows.h>
+
+int main(int argc, char* argv[])
+{
+	int iHeroHp = 100, iHeroAtk = 10, iHeroDef = 5;
+	int iMopHp = 100, iMopAtk = 5, iMopDef = 3;
+	int iMenu = -1;
+
+	printf("RPG\n");
+
+	while (1)
+	{
+		// 메인 메뉴
+		printf("메인 메뉴\n");
+		printf("새 게임(0), 불러오기(1), 돌아가기(기타)\n");
+		scanf_s("%d", &iMenu);
+
+		if (iMenu == 0)		// 새 게임
+		{
+			printf("새 게임\n");
+
+			while (1)
+			{
+				// 게임 메뉴
+				printf("게임 메뉴\n");
+				printf("던전입장(0), 나가기(기타)\n");
+				scanf_s("%d", &iMenu);
+
+				if (iMenu == 0)		// 던전 입장
+				{
+					printf("던전 입장\n");
+					while (1)
+					{
+						// 던전
+						printf("던전 메뉴\n");
+						printf("이동(0), 나가기(기타)\n");
+						scanf_s("%d", &iMenu);
+
+						if (iMenu == 0)		// 이동
+						{
+							printf("이동\n");
+							Sleep(2000);
+							system("cls");
+
+							while (1)
+							{
+								// 전투
+								printf("전투 메뉴\n");
+								printf("공격(0), 방어(1), 후퇴(기타)\n");
+								scanf_s("%d", &iMenu);
+
+								// 데미지 계산식
+								// 방어력 - 공격력
+								int iDamage = 0;
+
+								if (iMenu == 0) 		// 전투
+								{
+									printf("공격\n");
+
+									// 주인공 1회 공격
+									iDamage = iMopDef - iHeroAtk;
+									if (iDamage < 0)
+									{
+										iMopHp += iDamage;
+
+										printf("주인공이 몬스터를 공격합니다.\n");
+										printf("피해를 %d 입혔습니다.\n", iDamage);
+										printf("남은 체력 : %d\n", iMopHp);
+									}
+									else
+									{
+										printf("공격에 실패 하셨습니다.\n");
+									}
+
+									// 몬스터 1회 반격
+									iDamage = iHeroDef - iMopAtk;
+									if (iDamage < 0)
+									{
+										iHeroHp += iDamage;
+
+										printf("몬스터가 반격 합니다.\n");
+										printf("피해를 %d 입었습니다.\n", iDamage);
+										printf("남은 체력 : %d\n", iHeroHp);
+									}
+									else
+									{
+										printf("몬스터가 공격에 실패 하셨습니다.\n");
+									}
+
+								}
+								else if (iMenu == 1)		// 방어
+								{
+									printf("방어\n");
+
+									// 주인공이 방어(방어력 +2)
+									iDamage = (iHeroDef + 2) - iMopAtk;
+									// 몬스터가 반격
+									if (iDamage < 0)
+									{
+										iHeroHp += iDamage;
+										printf("몬스터가 반격합니다.\n");
+										printf("피해를 %d 입었습니다.\n", iDamage);
+										printf("남은 체력 : %d\n", iHeroHp);
+									}
+									else
+									{
+										printf("몬스터가 공격에 실패 하셨습니다.\n");
+									}
+								}
+								else                    // 후퇴
+								{
+									iDamage = iHeroDef - (iMopAtk - 2);
+									if (iDamage < 0)
+									{
+										iHeroHp += iDamage;
+
+										printf("몬스터가 반격 합니다.\n");
+										printf("피해를 %d 입었습니다.\n", iDamage);
+										printf("남은 체력 : %d\n", iHeroHp);
+									}
+									else
+									{
+										printf("몬스터가 공격에 실패 하셨습니다..\n");
+									}
+
+									printf("던전으로 이동.\n");
+									break;
+								}
+								if (iMopHp <= 0)
+								{
+									printf("전투에 승리하였습니다.\n");
+									break;
+								}
+							}
+						}
+						else                // 나가기
+						{
+							printf("게임 메뉴료 이동.\n");
+							break;
+						}
+					}
+
+				}
+				else               // 나가기 
+				{
+					printf("메인메뉴로 이동.\n");
+					break;
+				}
+			}
+
+		}
+		else                // 나가기
+		{
+			printf("게임을 종료합니다.\n");
+			break;
+		}
+	}
+
+
+	return 0;
+}
